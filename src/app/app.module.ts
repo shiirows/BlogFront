d009f42c6e88b44107bd0c449a2f01b347047c56
shiirows <mazoyeralexandre@hotmail.fr>
2021-09-21 13:49:26 +0200

    démarage du front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { ProfilComponent } from './profil/profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    ConnexionComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule
