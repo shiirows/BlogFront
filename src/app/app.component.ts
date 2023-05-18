4ae01bea8abe2e90898c52d96b86e42693358b64
Mazoyer <mazoyeralexandre@hotmail.fr>
2023-05-18 17:29:41 +0200

    add netlify.toml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blogFront';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
+  
 }
+
+
+ 
